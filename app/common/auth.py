1901efc5811f95eb5dd9e3c4b9fad25d325ef8ba
mgurg <34517378+mgurg@users.noreply.github.com>
2025-08-28 12:58:50 +0200

    Fake auth

---
+++ app/common/auth.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBasicCredentials, HTTPBearer
+from starlette.status import HTTP_401_UNAUTHORIZED
+security = HTTPBearer()
+
+
+def check_token(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    """
+    Function that is used to validate the token in the case that it requires it
+    """
+    token = credentials.credentials
+    if token is None:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Missing auth token")
+    if len(token) != 31:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Incorrect auth token")
+
+    return True
