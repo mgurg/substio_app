f91fd79a4ebd666c75d7f60a0e7a1eca96b08439
mgurg <34517378+mgurg@users.noreply.github.com>
2025-10-02 08:23:34 +0200

    clean-up

--- app/common/slack/factory.py
+++ app/common/slack/factory.py
@@ -1,10 +1,6 @@
-
 from app.common.slack.SlackNotifier import SlackNotifier
 from app.common.slack.SlackNotifierBase import SlackNotifierBase
 
 
-# ------------------------------
-# Dependency for FastAPI DI
-# ------------------------------
 def get_slack_notifier() -> SlackNotifierBase:
     return SlackNotifier()
